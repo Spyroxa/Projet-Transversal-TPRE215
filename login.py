06010f1aab2728ad3108fc2f1936e68d32fbdad2
Spyroxa <osulebihanaxel@gmail.com>
2022-03-31 14:54:25 +0200

    rajout BDD

--- login.py
+++ login.py
@@ -2,20 +2,20 @@ from flask import Flask, render_template, redirect, url_for, flash, request, ses
 from flask_wtf import FlaskForm
 from flask_bootstrap import Bootstrap
 from wtforms import StringField, SubmitField, BooleanField, IntegerField, SelectField, DecimalField, PasswordField, \
-    EmailField
+    EmailField,TextAreaField
 from wtforms.validators import DataRequired, NumberRange
 from connectBDD import DBSingleton
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'this is a secret'
+app.config['SECRET_KEY'] = 'this is not a secret'
 Bootstrap(app)
 
 
 class authform(FlaskForm):
-    mail = EmailField('mail', validators=[DataRequired()])
+    login = TextAreaField('mail', validators=[DataRequired()])
     MDP = PasswordField('Mot de passe', validators=[DataRequired()])
     submit = SubmitField('Submit')
-    session_MAIL = None
+    session_LOGIN = None
     Session_MDP = None
     Session_id = None
 
@@ -25,28 +25,28 @@ def log():
     cookie = False
     retourner = [None, None, None, cookie]
     if form.validate_on_submit():
-        mail = request.form.get('mail')
-        MDP = request.form.get('MDP')
-        sql = f"SELECT email FROM Compte WHERE email = '{mail}'"
+        login = request.form.get('login')
+        MDP = request.form.get('motDePasse')
+        sql = f"SELECT login FROM utilisateur WHERE login = '{login}'"
         db_instance = DBSingleton.Instance()
         temp = db_instance.query(sql)
         if len(temp) == 0:
             print('vide')
         else:
-            true_mail = temp[0][0]
-            if true_mail == mail:
-                sql = f"SELECT mdp FROM Compte WHERE email = '{mail}'"
+            true_login = temp[0][0]
+            if true_login == login:
+                sql = f"SELECT motDePasse FROM utilisateur WHERE motDePasse = '{login}'"
                 db_instance = DBSingleton.Instance()
                 temp = db_instance.query(sql)
                 password = temp[0][0]
                 """mot de passe prit part le insert"""
                 if password == MDP:
-                    sql = f"SELECT role FROM Compte WHERE email = '{mail}' AND mdp = '{MDP}'"
+                    sql = f"SELECT id.utilisateur AS ID FROM utilisateur WHERE login = '{login}' AND motDePasse = '{MDP}'"
                     db_instance = DBSingleton.Instance()
                     temp = db_instance.query(sql)
-                    role = temp[0][0]
+                    ID = temp[0][0]
                     cookie = True
-                    retourner = [role, mail, MDP, cookie]
+                    retourner = [ID, login, MDP, cookie]
                     session['user'] = {"info": retourner}
                     print(f"le mot de passe session est {retourner}")
                 else:
@@ -56,17 +56,14 @@ def log():
 def LogUser():
     cookie = log()
     form = authform()
-    role = cookie[0]
+    ID = cookie[0]
     print(cookie)
     session['user'] = {"info": cookie}
     print(session['user']['info'])
     title = 'login'
     result = render_template('login.html', form=form, title=title)
     if cookie[3] is True:
-        if role == 1:
             result = redirect('/admin')
-        elif role == 0:
-            result = redirect('/user')
     return result
 
 

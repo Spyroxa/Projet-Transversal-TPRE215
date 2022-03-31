b91776a83aabc1106f979705303975a7b9ca4979
leyohann <perezpierre76@gmail.com>
2022-03-31 15:46:00 +0200

    formulaire qui marche

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, Blueprint
 from flask_wtf import FlaskForm
+from flask_bootstrap import Bootstrap
 from wtforms import StringField, SubmitField, BooleanField, IntegerField, \
     SelectField, DecimalField, DateField
 from wtforms.validators import DataRequired, NumberRange
@@ -7,15 +8,17 @@ from connectBDD import DBSingleton
 
 if __name__ == '__main__':
     app = Flask(__name__)
+    Bootstrap(app)
     db = DBSingleton.Instance()
     app.config['SECRET_KEY'] = 'this is not a secret'
+
 
     class FormulaireCreationEntreprise(FlaskForm):
 
         nom = StringField("Nom de l'entreprise", validators=[DataRequired()])
         numSiret = StringField("numero de siret", validators=[DataRequired()])
-        adressePostale = StringField('Prix de la visite', validators=[DataRequired()])
-        codePostal = StringField("Date de l'étape", validators=[DataRequired()])
+        adressePostale = StringField("adresse principale de l'entreprise", validators=[DataRequired()])
+        codePostal = StringField("Code postal", validators=[DataRequired()])
         ville = StringField("ville de location", validators=[DataRequired()])
         description = StringField("description de l'entreprise", validators=[DataRequired()])
         valider = SubmitField('Valider')
@@ -23,6 +26,9 @@ if __name__ == '__main__':
 
     @app.route('/form', methods=['GET', 'POST'])
     def ajoutEntreprise():
-        render_template('login.html')
+        form = FormulaireCreationEntreprise()
+        if form.validate_on_submit():
+
+        return render_template('login.html', form=form)
 
 app.run(debug=True)
